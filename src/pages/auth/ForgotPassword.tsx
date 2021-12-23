c2a44e090971e65b01d431b59181e1ecda1d794d
elkarouani <elkarouani@gmail.com>
2021-12-23 19:00:24 +0100

    Initialize pages

---
+++ src/pages/auth/ForgotPassword.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPasswordPage() {
+    return (
+        <div>
+            <h1>Forgot Password Page</h1>
+        </div>
+    );
+}
---
+++ src/pages/auth/SignUp.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+}
