147369f77a4974e8bfb657ea585aa726415608cd
elkarouani <elkarouani@gmail.com>
2021-12-25 15:46:41 +0100

    Page loading animation Fixes #16

--- src/hooks/ContextManager.tsx
+++ src/hooks/ContextManager.tsx
@@ -1,26 +1,31 @@
+import Loading from '@/pages/Loading';
 import React, { Suspense } from 'react';
 
 const CONTEXT_REGISTER: Array<string> = [
-    './contexts/UserContext',
+	'./contexts/UserContext',
 ];
 
 const getProviders = () =>
-    CONTEXT_REGISTER.map(context_relative_path =>
-        React.lazy(() => import(/* @vite-ignore */`${context_relative_path}`))
-    );
+	CONTEXT_REGISTER.map(context_relative_path =>
+		React.lazy(() => {
+			return new Promise(resolve => {
+				setTimeout(() => resolve(import(/* @vite-ignore */`${context_relative_path}`)), 1000);
+			});
+		})
+	);
 
 interface ContextManagerProviderProps {
-    children: React.ReactNode,
+	children: React.ReactNode,
 };
 
 const ContextManagerProvider = ({ children }: ContextManagerProviderProps) => {
-    const providers = getProviders();
-    const providersToDOM = providers.reduce(
-        (Inner, Outer) => <Outer>{Inner}</Outer>,
-        children
-    );
+	const providers = getProviders();
+	const providersToDOM = providers.reduce(
+		(Inner, Outer) => <Outer>{Inner}</Outer>,
+		children
+	);
 
-    return <Suspense fallback={<div>Loading...</div>}>{providersToDOM}</Suspense>;
+	return <Suspense fallback={<Loading />}>{providersToDOM}</Suspense>;
 };
 
 export default ContextManagerProvider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
         'worm-primary': '#4A90E2',
         error: '#FB7C7C',
         success: '#A2CE38',
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        }
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in',
       }
     },
   },
