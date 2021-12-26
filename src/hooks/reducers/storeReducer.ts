a56415c1ae9f4a6836eb75a2bc1bdf453029eb9d
elkarouani <elkarouani@gmail.com>
2021-12-26 21:12:38 +0100

    Add json data for testing Fixes #30

--- src/hooks/reducers/storeReducer.ts
+++ src/hooks/reducers/storeReducer.ts
@@ -1,3 +1,6 @@
+import mostPurchasedProducts from "@/store/products/mostPurchasedProducts";
+import newProducts from "@/store/products/newProducts";
+import allServices from "@/store/services/allServices";
 import Product from "../entities/Product";
 import Service from "../entities/Service";
 import StoreInterface from "../interfaces/StoreInterface";
@@ -21,27 +24,56 @@ export default function storeReducer(state: StoreInterface, action: any) {
         },
       };
     case "GET_NEW_PRODUCTS":
+      const newProductsList = newProducts.map((productData: Product) => {
+        let product = new Product();
+        product.slug = productData.slug;
+        product.label = productData.label;
+        product.description = productData.description;
+        product.illustration = productData.illustration;
+        product.price = productData.price;
+        return product;
+      });
+
       return {
         ...state,
         products: {
           ...state.products,
-          newProducts: [...state.products.newProducts, new Product()],
+          newProducts: [...state.products.newProducts, ...newProductsList],
         },
       };
     case "GET_MOST_PURCHASED_PRODUCTS":
+      const mostPurchasedProductsList = mostPurchasedProducts.map((productData: Product) => {
+        let product = new Product();
+        product.slug = productData.slug;
+        product.label = productData.label;
+        product.description = productData.description;
+        product.illustration = productData.illustration;
+        product.price = productData.price;
+        return product;
+      });
+
       return {
         ...state,
         products: {
           ...state.products,
-          mostPurchasedProducts: [...state.products.mostPurchasedProducts, new Product()],
+          mostPurchasedProducts: [...state.products.mostPurchasedProducts, ...mostPurchasedProductsList],
         },
       };
     case "GET_ALL_SERVICES":
+      const allServicesList = allServices.map((serviceData: Service) => {
+        let service = new Service();
+        service.slug = serviceData.slug;
+        service.label = serviceData.label;
+        service.description = serviceData.description;
+        service.illustration = serviceData.illustration;
+        return service;
+      });
+
       return {
         ...state,
         services: {
           ...state.services,
-          allServices: [...state.services.allServices, new Service()],
+          allServices: [...state.services.allServices, ...allServicesList],
         },
       };
     default:
