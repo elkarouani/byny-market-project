edeae5698c9df5973764240b4412642beff81920
elkarouani <elkarouani@gmail.com>
2022-01-20 13:52:51 +0100

    Limit the minimum quantity when decreasing product in cart Fixes #67

--- src/tests/store/unit/DecreaseItemCart.spec.ts
+++ src/tests/store/unit/DecreaseItemCart.spec.ts
@@ -26,4 +26,21 @@ describe("Store/unit: Decrease item in cart", () => {
 
     expect(result.current.cart.cartItems[0].quantity).toBe(1);
   });
+  it("should not decrease quantity if quantity is 1", () => {
+    const { result } = renderHook(() => useStore());
+
+    act(() => {
+      result.current.cartContextActions.addProductToCart(new Product("test"));
+    });
+
+    expect(result.current.cart.cartItems.length).toEqual(1);
+    expect(result.current.cart.cartItems[0].quantity).toEqual(1);
+    expect(result.current.cart.cartItems[0].product.slug).toEqual("test");
+
+    act(() => {
+      result.current.cartContextActions.decreaseItemQuantity("test");
+    });
+
+    expect(result.current.cart.cartItems[0].quantity).toBe(1);
+  });
 });
