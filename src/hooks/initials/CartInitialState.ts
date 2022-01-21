97223e73d5336fb456fad2462a750f1608757971
elkarouani <elkarouani@gmail.com>
2022-01-21 16:29:46 +0100

    Fix: update cart initial state

--- src/hooks/initials/CartInitialState.ts
+++ src/hooks/initials/CartInitialState.ts
@@ -4,6 +4,7 @@ import CartInterface from "../interfaces/CartInterface";
 const cartInitialState: CartInterface = {
   cartItems: [],
   addProductToCart: (product: Product) => {},
+  isProductInCart: (productSlug: string) => false,
   increaseItemQuantity: (productSlug: string) => {},
   decreaseItemQuantity: (productSlug: string) => {},
   removeItemFromCart: (productSlug: string) => {}
