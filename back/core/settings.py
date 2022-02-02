bb7836b2a59a536def366fd26687f3fb58f639f4
elkarouani <elkarouani@gmail.com>
2022-02-02 18:47:13 +0100

    Implement new backend app Fixes #84
    Secure django secret key

--- back/core/settings.py
+++ back/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l0a)e#8e+k#8c4lc4w0$p9^*5$#uc1sggb(_%uly1b6ff(%%g3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
