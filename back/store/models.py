eccd3ee6618b4d61239bcb3180fe2d58cb41fe36
elkarouani <elkarouani@gmail.com>
2022-02-08 18:05:18 +0100

    Add required models to backend app Fixes #82

--- back/store/models.py
+++ back/store/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    label = models.CharField(max_length=100)
+    illustration = models.ImageField(upload_to='services')
+    description = models.TextField()
+
+
+class Product(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    label = models.CharField(max_length=100)
+    illustration = models.ImageField(upload_to='products')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_label = models.CharField(max_length=100)
+    unit_value = models.IntegerField()
+
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    illustration = models.ImageField(upload_to='categories')
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+class CartDetails(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Cart Details"
